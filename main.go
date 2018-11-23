6050e8da487b1bd12c2615bf1a2860829d4ff0fd
tamayika <tamayika40@gmail.com>
2018-11-23 15:24:22 +0900

    fix main.go command bug

--- main.go
+++ main.go
@@ -13,18 +13,9 @@ import (
 	"github.com/tamayika/gaq/pkg/gaq/query"
 )
 
-func main() {
-	data, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("Cannot read data from stdin. %v", err)
-	}
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "", string(data), parser.ParseComments)
-	if err != nil {
-		log.Fatalf("Cannot parse source. %v", err)
-	}
-	node := gaq.MustParseNode(f)
+var version = "dev"
 
+func main() {
 	rootCmd := &cobra.Command{
 		Use:   "gaq <Query>",
 		Short: "gaq is the cli tool to query ast node. STDIN needed as go code.",
@@ -34,8 +25,20 @@ Typical usage is
   cat <go file path> | gaq <Query>
 
 Please see details at https://github.com/tamayika/gaq`,
-		Args: cobra.ExactArgs(1),
+		Args:    cobra.ExactArgs(1),
+		Version: version,
 		Run: func(cmd *cobra.Command, args []string) {
+			data, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				log.Fatalf("Cannot read data from stdin. %v", err)
+			}
+			fset := token.NewFileSet()
+			f, err := parser.ParseFile(fset, "", string(data), parser.ParseComments)
+			if err != nil {
+				log.Fatalf("Cannot parse source. %v", err)
+			}
+			node := gaq.MustParseNode(f)
+
 			q := query.MustParse(args[0])
 			nodes := node.QuerySelectorAll(q)
 			for _, node := range nodes {
@@ -45,6 +48,7 @@ Please see details at https://github.com/tamayika/gaq`,
 			}
 		},
 	}
+	rootCmd.SetVersionTemplate(`{{printf "%s" .Version}}`)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
